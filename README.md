ef101acf0b1cca0aefcdd7b3d4253fe5b672dfe8
a24ma <62923767+a24ma@users.noreply.github.com>
2024-10-16 17:56:42 +0900

    py_full_template を追加

--- README.md
+++ README.md
@@ -7,6 +7,60 @@
 
 ## Introduction
 
-TODO:
+Python 用のテンプレート。
+
+主に vscode のスニペット登録用。
+
+### py_full_template
+
+Python Script 用のテンプレート。
+
+* 正常終了・異常終了時の、修了後待ち受け処理付き。
+    * bash を想定。
+* logging を想定したログ管理。
+    * 
 
 
+
+## スニペット用
+
+### 参考サイト
+
+* 変換ツール: https://migi.me/vsc_snippet/
+
+### Snippet 変数名
+
+* `TM_SELECTED_TEXT`: 現在選択しているテキスト
+* `TM_CURRENT_LINE`: 現在の行の内容
+* `TM_CURRENT_WORD`: 現在のカーソル位置の単語（もしくは空白）
+* `TM_LINE_INDEX`: 最初の行を０行目としたときの現在の行番号
+* `TM_LINE_NUMBER`: 最初の行を１行目としたときの現在の行番号
+* `TM_FILENAME`: 現在のドキュメントのファイル名
+* `TM_FILENAME_BASE`: 現在のドキュメントのファイル名（拡張子なし）
+* `TM_DIRECTORY`: 現在のドキュメントのディレクトリ
+* `TM_FILEPATH`: 現在のドキュメントのフルのファイルパス
+* `RELATIVE_FILEPATH`: 現在のドキュメントの相対（開いているワークスペースやフォルダに対する）ファイルパス
+* `CLIPBOARD`: クリップボードの内容
+* `WORKSPACE_NAME`: 開いているワークスペースまたはフォルダーの名前
+* `WORKSPACE_FOLDER`: 開いているワークスペースまたはフォルダーのパス
+* `CURRENT_YEAR`: 現在の年
+* `CURRENT_YEAR_SHORT`: 現在の年の下2桁
+* `CURRENT_MONTH`: 2桁の月 (例 '02')
+* `CURRENT_MONTH_NAME`: 月の正式名称（例：'July')
+* `CURRENT_MONTH_NAME_SHORT`: 月の略称（例：'Jul'）。
+* `CURRENT_DATE`: 2桁の日付(例：'08')
+* `CURRENT_DAY_NAME`: 曜日名 (例: 'Monday')
+* `CURRENT_DAY_NAME_SHORT`: 曜日の略称（例：'Mon')
+* `CURRENT_HOUR`: 24時間表示での現在時刻
+* `CURRENT_MINUTE`: 現在の分を2桁で表示
+* `CURRENT_SECOND`: 現在の秒を2桁で表示
+* `CURRENT_SECONDS_UNIX`: Unixエポックからの秒数
+* `RANDOM`: 10進数6桁の乱数
+* `RANDOM_HEX`: 16進数6桁の乱数
+* `UUID`: バージョン4のUUID
+* `BLOCK_COMMENT_START`: 出力例：C++の場合 /* またはHTMLの場合 <!--
+* `BLOCK_COMMENT_END`: 出力例：C++の場合 */ またはHTMLの場合 -->
+* `LINE_COMMENT`: 出力例：C++の場合 //
+* `1`～`9`: タブストップ$1～$9の内容
+* `CURSOR_INDEX`: マルチカーソルの最初の選択を0とした番号
+* `CURSOR_NUMBER`: マルチカーソルの最初の選択を1とした番号
---
+++ py_full_template/main.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################
+# region    <IMPORT>
+
+import pathlib
+from pathlib import Path
+
+# endregion </IMPORT>
+######################################
+# region    <USER_SETTINGS>
+
+log_package_name = "TODO:"  # Avoid __package__==None on main
+
+# endregion </USER_SETTINGS>
+######################################
+# region    <UTILITY>
+
+import click
+import logging
+import subprocess
+from rich import print
+from rich.console import Console
+from rich.logging import RichHandler
+
+log_trace_name = log_package_name + ".trace"  # logger with different logLevel
+suppress_lib_on_err = [click, pathlib]  # Suppressed lib.'s stacktrace
+
+log = logging.getLogger(log_package_name)
+log_trace = logging.getLogger(log_trace_name)
+console = Console(stderr=True)
+
+
+def set_logger(debug_mode, quiet_mode, trace_mode=False):
+    if debug_mode or trace_mode:
+        log_level = logging.DEBUG
+    elif quiet_mode:
+        log_level = logging.WARNING
+    else:
+        log_level = logging.INFO
+
+    log_fmt = "[%(name)10s] %(message)s"
+    log_datefmt = "[%X]"  # w/o date
+    # log_datefmt = "[%Y-%m-%d %X]" # w/ date
+    log_fmt = logging.Formatter(fmt=log_fmt, datefmt=log_datefmt)
+    log_hdl = RichHandler(rich_tracebacks=True, console=console)
+    log_hdl.setFormatter(log_fmt)
+
+    log.setLevel(log_level)
+    log.addHandler(log_hdl)
+    # log.addHandler(logging.FileHandler("log.txt"))
+
+    if trace_mode:
+        log_trace.setLevel(logging.DEBUG)
+    else:
+        log_trace.setLevel(logging.ERROR)
+
+
+def waitkey(wait_sec: int, output=print, **output_kwargs):
+    output("Press any key:", **output_kwargs)
+    cmd = f"read -t {wait_sec} -n 1"
+    subprocess.run(f"bash -c '{cmd}'", shell=True, cwd="/")
+
+
+# endregion </UTILITY>
+######################################
+# region    <MAIN>
+
+
+def main(**kwargs):
+    # TODO: main process
+    return True
+
+
+# endregion </MAIN>
+######################################
+# region    <CUI>
+
+
+@click.command()
+@click.option("--quiet", "-q", "quiet", is_flag=True)
+@click.option("--debug", "-d", "debug", is_flag=True)
+@click.option("--trace", "-dd", "trace", is_flag=True)
+@click.option("--wait_ok", "-wok", "wait_ok", type=int, default=5)
+@click.option("--wait_ng", "-wng", "wait_ng", type=int, default=180)
+def cmd(**kwargs):
+    set_logger(kwargs["debug"], kwargs["quiet"], kwargs["trace"])
+    wait_time = kwargs["wait_ok"]
+    wait_time_ng = kwargs["wait_ng"]
+    try:
+        res = main(**kwargs)
+        if res:
+            print(f"[bold][green]Complete.[/green][/bold]")
+        else:
+            print(f"[bold][red]Failed.[/red][/bold]")
+            wait_time = wait_time_ng
+    except Exception:
+        wait_time = wait_time_ng
+        # TODO: error process
+        console.print_exception(
+            show_locals=kwargs["trace"],
+            suppress=suppress_lib_on_err,
+        )
+    finally:
+        waitkey(
+            wait_time,
+            output=lambda s: print(f"[bold][red]{s}[/red][/bold]", end=""),
+        )
+
+
+# endregion </CUI>
+######################################
+
+if __name__ == "__main__":
+    cmd()
